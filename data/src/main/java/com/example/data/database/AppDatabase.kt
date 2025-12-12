8f0c6f27ca219b19651db386c1f92ed099963d89
nikita <nikitaguzenko2@gmail.com>
2025-12-13 01:30:57 +0500

    TE-001 AppDatabase.k delete useless imports

--- data/src/main/java/com/example/data/database/AppDatabase.kt
+++ data/src/main/java/com/example/data/database/AppDatabase.kt
@@ -1,20 +1,13 @@
 package com.example.data.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.data.database.converters.ListConverter
 import com.example.data.database.dao.QuestionDao
 import com.example.data.database.dao.TopicDao
 import com.example.data.database.entity.QuestionEntity
 import com.example.data.database.entity.TopicEntity
-import com.example.data.util.JsonLoader
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 @Database(
     entities = [
